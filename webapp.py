9290f47f2e9af9ec02fb2d35ecca530fbbeb7d83
Lavanya Satyan <lsatyan@ucsd.edu>
2017-09-01 22:37:39 -0700

    webapp.py

--- webapp.py
+++ webapp.py
@@ -141,7 +141,7 @@ def renderPage1():
 @app.route('/list')
 def renderList():
    message = request.form.get("message")
-   #login = session['user_data']['login']    
+   login = session['user_data']['login']    
    #mongo.test-db.messages.insert_one(
    #                {"message":message, 
    #                 "login":login })
@@ -151,7 +151,6 @@ def renderList():
 @app.route('/page2')
 def renderPage2():
     return render_template('page2.html')
-
 
 @github.tokengetter
 def get_github_oauth_token():
